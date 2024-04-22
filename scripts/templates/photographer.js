f8492d2c9b0eae3776e5642266690cbb0594a81a
oHminod <yvesm13@gmail.com>
2024-04-22 18:01:45 +0200

    démarrage, installation eslint, récupération des données, affichage page d'accueil, lien vers page photographe

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,31 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
+export function photographerTemplate(data) {
+    const { name, portrait, id, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const article = document.createElement("article");
+        const link = document.createElement("a");
+        link.setAttribute("href", `photographer.html?id=${id}`);
+        link.setAttribute("aria-label", `Voir la page de ${name}`);
+        const img = document.createElement("img");
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
+        const h2 = document.createElement("h2");
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        const h3 = document.createElement("h3");
+        h3.textContent = `${city}, ${country}`;
+        const p = document.createElement("p");
+        p.textContent = tagline;
+        const span = document.createElement("span");
+        span.textContent = `${price}€/jour`;
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(link);
+        article.appendChild(h3);
+        article.appendChild(p);
+        article.appendChild(span);
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM };
 }
