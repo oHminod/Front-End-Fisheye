9afdc6d78c585879c4882c781ef429876a54ba65
oHminod <yvesm13@gmail.com>
2024-04-24 23:52:52 +0200

    UI: filter menu style + refactor getMediaCardDOM

--- scripts/templates/media.js
+++ scripts/templates/media.js
@@ -15,68 +15,23 @@ export function mediaTemplate(media, index, sortedMedia, photographer) {
     const mainContent = document.getElementById("main");
     const lightBox = document.getElementById("lightbox");
 
-    function createMediaElement(type, source, title, index) {
-        const element = document.createElement(type);
-        element.setAttribute("src", `assets/media/${source}`);
-        element.setAttribute("alt", title);
-        element.setAttribute("tabindex", "0");
-        element.setAttribute(
-            "aria-label",
-            `Ouvrir ${type === "img" ? "l'image" : "la vidéo"} ${title}`
-        );
-        element.addEventListener("keydown", (event) => {
-            if (event.key === "Enter") {
-                event.preventDefault();
-                displayLightbox(sortedMedia, index);
-                mainContent.setAttribute("aria-hidden", "true");
-                lightBox.setAttribute("aria-hidden", "false");
-            }
-        });
-        element.addEventListener("click", () => {
-            displayLightbox(sortedMedia, index);
-            mainContent.setAttribute("aria-hidden", "true");
-            lightBox.setAttribute("aria-hidden", "false");
-        });
-        return element;
-    }
-
-    function createLikesElement(index, likes) {
-        const itemLikes = document.createElement("p");
-        const heart = document.createElement("span");
-        heart.setAttribute("class", "fa-regular fa-heart");
-        itemLikes.setAttribute("tabindex", "0");
-        itemLikes.setAttribute("aria-label", "liker l'image " + title);
-        heart.style.marginLeft = "0.5rem";
-        itemLikes.style.cursor = "pointer";
-
-        itemLikes.addEventListener("click", () => {
-            toggleLike(heart, index);
-            displayInfoCard(photographer, sortedMedia);
-        });
-
-        itemLikes.addEventListener("keydown", (event) => {
-            if (event.key === "Enter") {
-                event.preventDefault();
-                toggleLike(heart, index);
-                displayInfoCard(photographer, sortedMedia);
-            }
-        });
-        itemLikes.textContent = likes;
-        itemLikes.appendChild(heart);
-        return itemLikes;
-    }
-
-    function toggleLike(heart, index) {
-        if (heart.classList.contains("fa-regular")) {
-            heart.classList.replace("fa-regular", "fa");
-            sortedMedia[index].likes += 1;
-        } else {
-            heart.classList.replace("fa", "fa-regular");
-            sortedMedia[index].likes -= 1;
-        }
-    }
-
     function getMediaCardDOM() {
+        const article = createArticle();
+        const cartouche = createCartouche();
+
+        if (image) {
+            const img = createImageElement();
+            article.appendChild(img);
+        } else if (video) {
+            const videoElement = createVideoElement();
+            article.appendChild(videoElement);
+        }
+
+        article.appendChild(cartouche);
+        return article;
+    }
+
+    function createArticle() {
         const article = document.createElement("article");
         article.setAttribute("class", "media-card");
         article.id = mediaId;
@@ -84,34 +39,98 @@ export function mediaTemplate(media, index, sortedMedia, photographer) {
         article.setAttribute("data-date", date);
         article.setAttribute("data-price", mediaPrice);
         article.setAttribute("data-index", index);
+        return article;
+    }
 
-        if (image) {
-            const img = createMediaElement("img", image, title, index);
-            article.appendChild(img);
-        } else if (video) {
-            const videoElement = createMediaElement(
-                "video",
-                video,
-                title,
-                index
-            );
-            videoElement.setAttribute(
-                "poster",
-                `assets/media/poster/${video.slice(0, -4)}.jpg`
-            );
-            article.appendChild(videoElement);
+    function createImageElement() {
+        const img = document.createElement("img");
+        img.setAttribute("src", `assets/media/${image}`);
+        img.setAttribute("alt", title);
+        img.setAttribute("tabindex", "0");
+        img.setAttribute("aria-label", "Ouvrir l'image " + title);
+        img.addEventListener("keydown", handleMediaKeydown);
+        img.addEventListener("click", handleMediaClick);
+        return img;
+    }
+
+    function createVideoElement() {
+        const posterUrl = `assets/media/poster/${video.slice(0, -4)}.jpg`;
+        const videoElement = document.createElement("video");
+        videoElement.setAttribute("src", `assets/media/${video}`);
+        videoElement.setAttribute("alt", title);
+        videoElement.setAttribute("poster", posterUrl);
+        videoElement.setAttribute("tabindex", "0");
+        videoElement.setAttribute("aria-label", "Ouvrir la vidéo " + title);
+        videoElement.addEventListener("keydown", handleMediaKeydown);
+        videoElement.addEventListener("click", handleMediaClick);
+        return videoElement;
+    }
+
+    function handleMediaKeydown(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            displayLightbox(sortedMedia, index);
+            mainContent.setAttribute("aria-hidden", "true");
+            lightBox.setAttribute("aria-hidden", "false");
         }
+    }
 
+    function handleMediaClick() {
+        displayLightbox(sortedMedia, index);
+        mainContent.setAttribute("aria-hidden", "true");
+        lightBox.setAttribute("aria-hidden", "false");
+    }
+
+    function createCartouche() {
         const cartouche = document.createElement("div");
         cartouche.setAttribute("class", "cartouche");
         const h2 = document.createElement("h2");
         h2.textContent = title;
-        const itemLikes = createLikesElement(index, likes);
+        const itemLikes = createItemLikes();
         cartouche.appendChild(h2);
         cartouche.appendChild(itemLikes);
+        return cartouche;
+    }
 
-        article.appendChild(cartouche);
-        return article;
+    function createItemLikes() {
+        function handleLikesClick() {
+            toggleLike();
+            displayInfoCard(photographer, sortedMedia);
+        }
+
+        function handleLikesKeydown(event) {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                toggleLike();
+                displayInfoCard(photographer, sortedMedia);
+            }
+        }
+
+        function toggleLike() {
+            if (heart.classList.contains("fa-regular")) {
+                heart.classList.replace("fa-regular", "fa");
+                sortedMedia[index].likes += 1;
+            } else {
+                heart.classList.replace("fa", "fa-regular");
+                sortedMedia[index].likes -= 1;
+            }
+            itemLikes.textContent = sortedMedia[index].likes;
+            itemLikes.appendChild(heart);
+        }
+
+        const itemLikes = document.createElement("p");
+        const heart = document.createElement("span");
+        heart.setAttribute("class", "fa-regular fa-heart");
+        itemLikes.setAttribute("tabindex", "0");
+        itemLikes.setAttribute("aria-label", "liker l'image " + title);
+        heart.style.marginLeft = "0.5rem";
+        itemLikes.style.cursor = "pointer";
+        itemLikes.addEventListener("click", handleLikesClick);
+        itemLikes.addEventListener("keydown", handleLikesKeydown);
+        itemLikes.textContent = likes;
+        itemLikes.appendChild(heart);
+
+        return itemLikes;
     }
 
     return {
